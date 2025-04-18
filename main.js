8aff7493f8148e30d598fb3209276e8f60f94018
mohamed azoz <125761608+MohamedAzoz@users.noreply.github.com>
2025-04-18 16:48:50 +0200

    main

--- main.js
+++ main.js
@@ -9,15 +9,19 @@ let false_item=document.querySelector(".false")
 email.addEventListener('focus',function(){
     correct.classList.replace('show','hidden')
     false_item.classList.replace('show','hidden')
+    email.style.border='2px solid hsl(223, 100%, 88%)'
 })
 submit.addEventListener('click',function(){
     let em=/\w+@\w+\.com/ig;
     if(em.test(email.value)){
         correct.classList.replace('hidden','show')
         console.log("correct");
+        email.style.border='2px solid rgb(14, 207, 14)'
+
         // correct.classList.add()
     }else{
         false_item.classList.replace('hidden','show')
+        email.style.border='2px solid hsl(354, 100%, 66%)'
         console.log("not correct");
 
     }
